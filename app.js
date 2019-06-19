939611a235171c9ab2a346fdd4fd9a9071361108
apenkov <apenkov@spbfiller.com>
2019-06-19 11:13:18 +0300

    add second dice to game

--- app.js
+++ app.js
@@ -14,31 +14,38 @@ const RESET_VALUE = 1;
 let scores = [0, 0];
 let activePlayer = 0;
 let current = 0;
-const diceElement = document.querySelector('.dice');
+const diceElement_1 = document.querySelector('#dice_1');
+const diceElement_2 = document.querySelector('#dice_2');
 
 const initGame = () => {
   document.querySelector('#current-0').textContent = 0;
   document.querySelector('#current-1').textContent = 0;
   document.querySelector('#score-0').textContent = 0;
   document.querySelector('#score-1').textContent = 0;
-  diceElement.style.display = 'none';
-  scores = [0, 0]
+  diceElement_1.style.display = 'none';
+  diceElement_2.style.display = 'none';
+  scores = [0, 0];
+  current = 0;
 }
 
 initGame();
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
-  let dice = Math.floor(Math.random() * 6) + 1;
+  let dice_1 = Math.floor(Math.random() * 6) + 1;
+  let dice_2 = Math.floor(Math.random() * 6) + 1;
+  console.log(`${dice_1} ${dice_2}`)
 
-  diceElement.src = `dice-${dice}.png`;
-  diceElement.style.display = 'block';
+  diceElement_1.src = `dice-${dice_1}.png`;
+  diceElement_2.src = `dice-${dice_2}.png`;
 
-  if (dice !== RESET_VALUE) {
-    current += dice;
+  diceElement_1.style.display = 'block';
+  diceElement_2.style.display = 'block';
+
+  if (dice_1 !== RESET_VALUE && dice_2 !== RESET_VALUE) {
+    current += dice_1 + dice_2;
     document.getElementById('current-'+activePlayer).textContent = current;
-
     if (scores[activePlayer] + current >= 20) {
-      alert(`Player ${activePlayer} won!!!`);
+       alert(`Player ${activePlayer} won!!!`);
     }
     
   } else {
@@ -51,7 +58,8 @@ const changePlayer = () => {
   document.getElementById('current-'+activePlayer).textContent = 0;
   document.querySelector(`.player-${activePlayer}-panel`).classList.toggle('active');
   activePlayer = +!activePlayer;
-  diceElement.style.display = 'none';
+  diceElement_1.style.display = 'none';
+  diceElement_2.style.display = 'none';
   document.querySelector(`.player-${activePlayer}-panel`).classList.toggle('active');
 }
 
