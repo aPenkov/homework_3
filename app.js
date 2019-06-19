b1ce742cbeb9ffa797733671185ddeffa9f23510
apenkov <apenkov@spbfiller.com>
2019-06-19 16:11:55 +0300

    added gamer constructor and added prompt dialogs, now users must enter thir players names

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /*
 GAME RULES:
 
-- The game has 2 players, playing in rounds
+- The game has 2 playersList, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
@@ -11,22 +11,50 @@ GAME RULES:
 
 const RESET_VALUE = 2;
 let scores = [0, 0];
-let activePlayer = 0;
+let activePlayerId = 0;
 let current = 0;
 const diceElement_1 = document.querySelector('#dice_1');
 const diceElement_2 = document.querySelector('#dice_2');
 let scoresForWin;
+let playersList = [];
+
+function Gamer(name,id) {
+  this.score = 0;
+  this.name = name;
+  this.id = id;
+
+  this.getScore = () =>{
+    return this.score;
+  }
+
+  this.setScore = (score) => {
+    this.score += score;
+    return this.score;
+  }
+  this.resetScore = () => {
+    this.score = 0
+  }
+}
+
+
 
 const initGame = () => {
+  const namePlayer1 = prompt('Кто  будет играть первым?');
+  const namePlayer2 = prompt('А вторым?');
+  const player1 = new Gamer(namePlayer1,0);
+  const player2 = new Gamer(namePlayer2,1);
+  playersList = [player1, player2];
+  document.querySelector('#name-0').textContent = player1.name;
+  document.querySelector('#name-1').textContent = player2.name;
+
   document.querySelector('#current-0').textContent = 0;
   document.querySelector('#current-1').textContent = 0;
   document.querySelector('#score-0').textContent = 0;
   document.querySelector('#score-1').textContent = 0;
-  scoresForWin = parseInt(document.getElementById('input-limit').value)
-  if (scoresForWin == NaN) {
+  scoresForWin = +document.getElementById('input-limit').value
+  if (scoresForWin == 0) {
     scoresForWin = 100
   }
-  console.log(scoresForWin)
   diceElement_1.style.display = 'none';
   diceElement_2.style.display = 'none';
   scores = [0, 0];
@@ -48,9 +76,10 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
   if (dice_1 !== RESET_VALUE && dice_2 !== RESET_VALUE && dice_1 !== dice_2) {
     current += dice_1 + dice_2;
-    document.getElementById('current-'+activePlayer).textContent = current;
-    if (scores[activePlayer] + current >= scoresForWin) {
-       alert(`Player ${activePlayer} won!!!`);
+    document.getElementById('current-'+activePlayerId).textContent = current;
+    playersList[activePlayerId].setScore(current)
+    if (scores[activePlayerId] + current >= scoresForWin) {
+       alert(`Player ${activePlayerId} won!!!`);
     }
     
   } else {
@@ -60,17 +89,18 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
 const changePlayer = () => {
   current = 0;
-  document.getElementById('current-'+activePlayer).textContent = 0;
-  document.querySelector(`.player-${activePlayer}-panel`).classList.toggle('active');
-  activePlayer = +!activePlayer;
+  playersList[activePlayerId].resetScore();
+  document.getElementById('current-'+activePlayerId).textContent = 0;
+  document.querySelector(`.player-${activePlayerId}-panel`).classList.toggle('active');
+  activePlayerId = +!activePlayerId;
   diceElement_1.style.display = 'none';
   diceElement_2.style.display = 'none';
-  document.querySelector(`.player-${activePlayer}-panel`).classList.toggle('active');
+  document.querySelector(`.player-${activePlayerId}-panel`).classList.toggle('active');
 }
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
-  scores[activePlayer] += current;
-  document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer];
+  scores[activePlayerId] += current;
+  document.querySelector(`#score-${activePlayerId}`).textContent = scores[activePlayerId];
   changePlayer();
 });
 
